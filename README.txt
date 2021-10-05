58aaa3f50a88c4a8ac77e721b9bc07b02c045a2a
Rodrigo Novas <rodrigo.novas@skf.com>
2021-10-05 11:30:01 -0300

    Add tp 1 and tp 2

---
+++ README.txt
@@ -0,0 +1,6 @@
+## TRABAJOS PRACTICOS UTN 
+
+## Lenguajes utilizados: C
+
+
+## Como correr el proyecto? importar archivo .cbp dentro del compilador/ide
---
+++ TP[1]/Calculadora.h
@@ -0,0 +1,7 @@
+float usuarioNumero(void    ); // funcion que retorna numero
+float sumaParametros(float a, float b); // suma de dos variables
+float restaParametros(float a, float b); // resta de dos variables
+float multiplicarParametros(float a, float b); // multiplicacion de dos variables
+float dividirParametros(float a, float b); // division de dos variables
+int factorialNumero(float n); //calcular factorial A
+int factorialNumero2(float n); //calcular factorial B
